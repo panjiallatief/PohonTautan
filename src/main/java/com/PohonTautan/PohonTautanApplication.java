1675bcee8ef7c2d2204c20f3f0424c2594632545
Panji Maulana Al Latief <65167200+panjiallatief@users.noreply.github.com>
2023-12-07 13:29:54 +0700

    first commit

---
+++ src/main/java/com/PohonTautan/PohonTautanApplication.java
@@ -0,0 +1,13 @@
+package com.PohonTautan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PohonTautanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PohonTautanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PohonTautan/PohonTautanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PohonTautan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PohonTautanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
