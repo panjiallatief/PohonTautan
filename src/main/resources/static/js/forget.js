6b92286a83fcc6bd3d850e34813b7f6b3b068d18
Panji Maulana Al Latief <65167200+panjiallatief@users.noreply.github.com>
2023-12-14 10:32:16 +0700

    1

--- src/main/resources/static/js/forget.js
+++ src/main/resources/static/js/forget.js
@@ -1,7 +1,7 @@
 $("#forgetpas").on('click', function(){
     var ema =  window.location.href;
     var mm = ema.substring(ema.lastIndexOf("/")+1, ema.length)
-    var ma = ema.substring(0, ema.lastIndexOf("/"))
+    var ma = ema.substring(0, ema.lastIndexOf("/pagerespas/"))
     console.log(ema)
     console.log(mm)
     console.log(ma)
