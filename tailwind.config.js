777cee49e8d9ab3a15070eefa68f46848e77428f
Panji Maulana Al Latief <65167200+panjiallatief@users.noreply.github.com>
2023-12-28 14:36:05 +0700

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  darkMode: ["class"],
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.html", "./srcc/**/*.js"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
