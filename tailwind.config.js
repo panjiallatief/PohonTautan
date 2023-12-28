670cf0ecfff1d6c5b1d2cbc91c52311f82f8b042
Panji Maulana Al Latief <65167200+panjiallatief@users.noreply.github.com>
2023-12-28 15:09:55 +0700

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   content: ["./src/**/*.html", "./srcc/**/*.js"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
   },
   plugins: [
     require('tailwindcss'),
